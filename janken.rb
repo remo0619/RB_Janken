68b9bc11a301c6d6018530adf048ee0a8074f2aa
remo0619 <ichinohe@aomori-innovators.com>
2026-02-04 07:20:52 +0900

    finish

--- janken.rb
+++ janken.rb
@@ -1,4 +1,5 @@
-jan_judge = true # 勝敗・あいこ判定フラグ
+jan_judge = "" # じゃんけん判定フラグ
+hoi_judge = "" # あっちむいてホイ判定フラグ
 
 def janken(player_hand, computer_hand) # じゃんけん関数
   hands = ["グー", "チョキ", "パー"]
@@ -9,13 +10,13 @@ def janken(player_hand, computer_hand) # じゃんけん関数
   puts "--------------------------------------------"
 
   if player_hand == computer_hand
-    return false
+    return "あいこ"
   elsif (player_hand == 0 && computer_hand == 1) ||
         (player_hand == 1 && computer_hand == 2) ||
         (player_hand == 2 && computer_hand == 0)
-    return true
+    return "勝ち"
   else
-    return true
+    return "負け"
   end
 end
 
@@ -26,10 +27,16 @@ def hoi(player_direction, computer_direction) # あっちむいてホイ関数
   puts "あなた: #{direction[player_direction]}"
   puts "Ruby  : #{direction[computer_direction]}"
   puts "--------------------------------------------"
+
+  if player_direction == computer_direction
+    return "勝負あり"
+  else
+    return ""
+  end
 end
 
 loop do
-  if jan_judge == true
+  if !(jan_judge == "あいこ")
     puts "じゃんけん..."
   else
     puts "あいこで..."
@@ -42,7 +49,7 @@ loop do
 
   case player_hand
   when 0, 1, 2
-    if jan_judge == true
+    if !(jan_judge == "あいこ")
       puts "ポン！"
     else
       puts "ショ！"
@@ -50,15 +57,22 @@ loop do
 
     jan_judge = janken(player_hand, computer_hand)
 
-    if jan_judge == true
+    if jan_judge == "勝ち" || jan_judge == "負け"
       puts "あっちむいて〜"
+      puts "0(上) 1(下) 2(左) 3(右)"
       computer_direction = rand(0..3)
       player_direction = gets.to_i
 
       case player_direction
       when 0, 1, 2, 3
         hoi_judge = hoi(player_direction, computer_direction)
-
+        if hoi_judge == "勝負あり" && jan_judge == "勝ち"
+          puts "あなたの勝ちです！"
+          break
+        elsif hoi_judge == "勝負あり" && jan_judge == "負け"
+          puts "あなたの負けです！"
+          break
+        end
       else
         puts "規定の値(0,1,2,3)を入力しなさい"
       end
